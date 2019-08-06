6721f28b04bdaf6a6e8814a7b21ab45b05cd3988
YO!CHI KIKUCHI <i@yo1000.com>
2019-08-06 19:50:54 +0900

    Update site meta-data

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = {
   siteMetadata: {
-    title: 'Ghost',
-    description: 'The professional publishing platform',
-    siteUrl: 'https://gatsby-casper.netlify.com', // full path to blog - no ending slash
+    title: 'talk2wall',
+    description: 'Then go talk to a wall',
+    siteUrl: 'https://www.yo1000.com', // full path to blog - no ending slash
   },
   mapping: {
     'MarkdownRemark.frontmatter.author': 'AuthorYaml',
@@ -56,7 +56,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-canonical-urls',
       options: {
-        siteUrl: 'https://gatsby-casper.netlify.com',
+        siteUrl: 'https://www.yo1000.com',
       },
     },
     'gatsby-plugin-emotion',
